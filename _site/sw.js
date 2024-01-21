76d5fe6ffe00c90090251c16db7f80f9b8b6f6bd
changhachangha <wjdckdgk1256@gmail.com>
2024-01-21 23:11:07 +0900

    설정 변경

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705832672';
+const cacheName = 'chirpy-1705844455';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
