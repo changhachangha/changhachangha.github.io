ae1ea0836d4aebeea6af3991f0de0de66141fcc5
changhachangha <wjdckdgk1256@gmail.com>
2024-01-22 20:46:19 +0900

    올라가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705847576';
+const cacheName = 'chirpy-1705923967';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
